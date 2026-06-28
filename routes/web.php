37aa31b1facebf5c7cd67f0e4dee7263b14d7f80
amirhussainnaweed <amirhussainnaweed@gmail.com>
2026-06-28 10:25:18 +0430

    feat: created route to make communication between backend and frontend

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\WishlistController;
 
 Route::get('/', function () {
     return Inertia::render('Home/Index');
@@ -49,6 +50,9 @@ Route::get('/order-details', function () {
 Route::get('/settings', function () {
     return Inertia::render('Account/Settings');
 });
+
+Route::post('/wishlist', [WishlistController::class, 'store']);
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard/Index');
     })->middleware(['auth', 'verified'])->name('dashboard');
